67fd4439b3bc923587b00ac1ffad8a2863853934
Gourav Sharma <sgourav533@gmail.com>
2021-08-22 18:18:17 +0530

    database init using spring (data.sql)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category(description) VALUES ('AMERICAN');
+INSERT INTO category(description) VALUES ('INDIAN');
+INSERT INTO category(description) VALUES ('ITALIAN');
+INSERT INTO category(description) VALUES ('MEXICAN');
+INSERT INTO unit_of_measure(description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure(description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure(description) VALUES ('Cup');
+INSERT INTO unit_of_measure(description) VALUES ('Pinch');
+INSERT INTO unit_of_measure(description) VALUES ('Ounce');
