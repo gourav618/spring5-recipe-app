6852166764d442ecdf1eacb4c07bc2a9bd17d194
Gourav Sharma <sgourav533@gmail.com>
2021-08-22 17:13:35 +0530

    added 1-*/*-1 JPA relation b/w recipe and Ingredient

--- src/main/java/com/recipe/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/recipe/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.recipe.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -23,6 +24,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
