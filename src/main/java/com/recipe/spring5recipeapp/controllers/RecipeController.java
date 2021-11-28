61d8244dee90650abbf674227f3a12a98e441502
GouravSharma <sgourav533@gmail.com>
2021-11-28 21:33:11 +0530

    added create new recipe form and controller methods and updated bootstrap cdn link

--- src/main/java/com/recipe/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/com/recipe/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.recipe.spring5recipeapp.controllers;
 
+import com.recipe.spring5recipeapp.commands.RecipeCommand;
 import com.recipe.spring5recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,4 +26,20 @@ public class RecipeController {
 
         return "recipe/show";
     }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
